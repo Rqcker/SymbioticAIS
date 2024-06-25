1904b1fd3bb092f6816505c06592bab723358714
acse-js3623 <junhao@usa.com>
2024-06-25 23:04:56 +0100

    release v0.2.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sais',
-    version='0.2.6',
+    version='0.2.7',
     packages=find_packages(),
     description='Symbiotic Artificial Immune Systems',
     long_description=open('README.md').read(),
