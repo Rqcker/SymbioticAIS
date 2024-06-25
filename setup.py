e96719a8e8dea464f76347d8ebc8ec9bdb1b9f43
Rqcker <songjh.john@gmail.com>
2024-06-25 22:41:38 +0100

    release v0.2.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sais',
-    version='0.2.4',
+    version='0.2.5',
     packages=find_packages(),
     description='Symbiotic Artificial Immune Systems',
     long_description=open('README.md').read(),
